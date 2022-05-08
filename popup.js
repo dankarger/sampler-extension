35b3aa4dd342e3e4c11dfe1a3c8948d3d9c3825e
dankarger <karger.dan@gmail.com>
2022-05-08 16:30:12 +0300

    adding visualizer

--- popup.js
+++ popup.js
@@ -1,34 +1,15 @@
 /*global chrome*/
-// chrome.downloads.download({url: "https://pixijs.download/release/pixi.js"},
-//    );
-// import * as PIXI from 'pixi.js'
-// const PIXI = require('pixi.js')
-// import { ShaderSystem, Renderer } from '@pixi/core';
-//
-// import { install } from '@pixi/unsafe-eval';
 
-// Apply the patch to PIXI
-// install({ ShaderSystem });
 
-// const Application = PIXI.Application
-//
-// const app = new Application({
-//     width:600,
-//     height:300,
-//     transparent: false,
-//     antialias: true
-// });
-// //
-// app.renderer.backgroundColor = 'red';
-// app.renderer.resize(window.innerWidth,window.innerHeight)
-// app.renderer.view.style.position = 'absolute';
-// document.body.appendChild(app.view)
+const visualizer = document.getElementById('visualizer')
 
 const mainDiv = document.getElementById('main')
 function init() {
     const audioCtx = new AudioContext();
-
-
+    const analyzerNode = new AnalyserNode(audioCtx, {fftSize: 256})
+    const gainNode = new GainNode(audioCtx, {gain: 1})
+    gainNode.connect(audioCtx.destination);
+    analyzerNode.connect(gainNode)
     const I = document.getElementById('I');
     const II = document.getElementById('II');
     const III = document.getElementById('III');
@@ -52,25 +33,71 @@ function init() {
             if (audioCtx.state === 'suspended') audioCtx.resume()
             const audio = new Audio(sound.source);
             const source = audioCtx.createMediaElementSource(audio);
+            source.connect(analyzerNode)
             source.connect(audioCtx.destination);
-            audioList.push(audio)
-            animateBackground()
+            audioList.push(source)
+            console.log(audio.duration)
+            let sampleDuration
+            audio.addEventListener('loadedmetadata', function(){
+                // Obtain the duration in seconds of the audio file (with milliseconds as well, a float value)
+                let duration = audio.duration;
+
+                // example 12.3234 seconds
+                console.log("The duration of the song is of: " + duration + " seconds");
+                sampleDuration=duration
+                animateBackground(Math.floor(sampleDuration)*1000)
+                // Alternatively, just display the integer value with
+                // parseInt(duration)
+                // 12 seconds
+            },false);
+
+
             audio.play();
+
+
         })
         sound.name.innerText = sound.label
+
     });
 
     stopButton.addEventListener('click', () => {
         audioList.forEach(sound => sound.volume = 0)
 
     })
+    function drawVisualizer () {
+        requestAnimationFrame(drawVisualizer)
+
+        const bufferLength = analyzerNode.frequencyBinCount;
+        const dataArray = new Uint8Array(bufferLength);
+        analyzerNode.getByteFrequencyData(dataArray);
+        const width = visualizer.width;
+        const height = visualizer.height;
+        const barWidth = width / bufferLength;
+
+        const canvasContext = visualizer.getContext('2d')
+        canvasContext.clearRect(0, 0, width, height);
+
+        dataArray.forEach((item, index) => {
+            const y = item / 255 * height / 2
+            const x = barWidth * index;
+
+            canvasContext.fillStyle = `hsl(${y / height * 400}, 100%, 50%)`
+            canvasContext.fillRect(x, height - y, barWidth, y)
+        })
+    }
+    drawVisualizer()
 }
 
 init();
 
-function animateBackground() {
+function animateBackground(timeout) {
+    console.log('g',timeout)
    mainDiv.classList.toggle('animate');
    setTimeout(()=>{
        mainDiv.classList.toggle('animate');
-   },700)
+   },timeout)
+}
+function stopAnimate() {
+    mainDiv.classList.toggle('animate');
+
 }
